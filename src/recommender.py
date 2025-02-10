1998472c2e42cf4982a4c58fc5b2f2d0bc830398
Silas <smanales@uwo.ca>
2025-02-10 17:58:54 -0500

    refactor: accommodated recommender.py to be general with column names depending on filter type

--- src/recommender.py
+++ src/recommender.py
@@ -2,10 +2,11 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 
 class Recommender:
-    def __init__(self, df, tfidf_matrix):
+    def __init__(self, df, tfidf_matrix, filter_type):
         self.df = df
+        self.filter_type = filter_type
         self.similarity_matrix = cosine_similarity(tfidf_matrix, tfidf_matrix)
-        self.indices = pd.Series(df.index, index=df['name']).drop_duplicates()
+        self.indices = pd.Series(df.index, index=df[self.filter_type]).drop_duplicates()
 
     def recommend(self, title, num_recommendations=5):
         """Returns a list of recommended TV shows based on a given title. default=5"""
@@ -16,7 +17,7 @@ class Recommender:
         similarity_scores = list(enumerate(self.similarity_matrix[idx]))
         similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)[1:num_recommendations + 1]
         tv_show_indices = [i[0] for i in similarity_scores]
-        return self.df['name'].iloc[tv_show_indices].tolist()
+        return self.df[self.filter_type].iloc[tv_show_indices].tolist()
 
 # Usage:
 # recommender = Recommender(df, tfidf_matrix)
